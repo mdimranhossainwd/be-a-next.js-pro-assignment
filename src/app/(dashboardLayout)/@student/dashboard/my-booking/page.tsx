21aea980e3a577307a52f38c78e288c5ce901b3b
mdimranhossainwd <mdimranhossain7498@gmail.com>
2026-01-29 18:35:06 +0600

    feat: Add to students dashbaord booking section ui added

--- src/app/(dashboardLayout)/@student/dashboard/my-booking/page.tsx
+++ src/app/(dashboardLayout)/@student/dashboard/my-booking/page.tsx
@@ -1,3 +1,10 @@
-export default function MyBooking() {
-  return <div>My Booking Page</div>;
+// app/(dashboard)/my-booking/page.tsx
+import { DataTable1 } from "@/components/data-table1";
+import { bookingService } from "@/service/booking.service";
+
+export default async function MyBooking() {
+  const { data: bookings } = await bookingService.getStudentBookings();
+  console.log(bookings);
+
+  return <DataTable1 bookings={bookings} />;
 }
