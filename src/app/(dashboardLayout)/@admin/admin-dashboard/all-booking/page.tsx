5111cedf51d368b3418b4d3fb8ebb33b2e9c0179
mdimranhossainwd <mdimranhossain7498@gmail.com>
2026-01-30 07:02:10 +0600

    feat: add to all booking card showing ui

--- src/app/(dashboardLayout)/@admin/admin-dashboard/all-booking/page.tsx
+++ src/app/(dashboardLayout)/@admin/admin-dashboard/all-booking/page.tsx
@@ -1,8 +1,12 @@
+import AdminBookingTable from "@/components/booking-table";
 import { bookingService } from "@/service/booking.service";
 
 export default async function AllBookingsPages() {
-  const { data: users } = await bookingService.getAllBookings();
-  console.log(users);
+  const { data: bookings } = await bookingService.getAllBookings();
 
-  return <div></div>;
+  return (
+    <div>
+      <AdminBookingTable bookings={bookings} />
+    </div>
+  );
 }
